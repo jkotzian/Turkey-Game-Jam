352951f96268dab4b748c793191f171504427e10
James Kotzian <jamezbond114@gmail.com>
2014-11-15 03:25:13 -0500

    Added co-op player

--- src/components.js
+++ src/components.js
@@ -11,40 +11,77 @@ Crafty.c('Actor', {
 
 Crafty.c('Ground', {
 	init: function() {
-		this.requires('Actor, Color')
+		this.requires('Actor, Color, Solid')
 			.color('rgb(20,125,40)');
-	},
-});
-
-Crafty.c('Door1', {
-	init: function() {
-		this.requires('Actor, Color')
-			.color('black')
-			.attr({w: 50, h:100});
-	},
-});
-
-Crafty.c('Player' , {
-	init: function() {
-		this.requires('2D, Canvas, Color, Gravity') 
-		  .attr({w: 50, h: 50})
-		  //.twoway(4)
-		  .gravity('Ground')
-		  .gravityConst(.5);
 	}
 });
 
 Crafty.c('Player1' , {
 	init: function() {
-		this.requires('Player, Multiway')
-		  .attr({x: 0, y: 0})
-		  .color('red')
-		  .multiway(10, {
+		this.requires('2D, Canvas, Color, Multiway, Gravity, Collision, Solid')
+		.attr({x: 0, y: 0, w: 50, h: 50})
+		.color('red')
+		.multiway(7, {
 			UP_ARROW: -90,
 			LEFT_ARROW: 180,
 			RIGHT_ARROW: 0,
-		  })
-		  .gravity('Ground')
-		  .gravityConst(.5);
-	}
+		})
+		.gravity('Solid')
+		.gravityConst(.3)
+		.stopOnSolids();
+	},
+
+	 // Registers a stop-movement function to be called when
+	// this entity hits an entity with the "Solid" component
+	stopOnSolids: function() {
+		console.log('Hey!');
+		this.onHit('Solid', this.stopMovement);
+		return this;
+	},
+
+	// Stops the movement
+	stopMovement: function() {
+		console.log('HI!');
+		this._speed = 0;
+		if (this._movement) {
+			this.x -= this._movement.x;
+			this.y -= this._movement.y;
+		}
+	},
 });
+
+Crafty.c('Player2' , {
+	init: function() {
+		this.requires('2D, Canvas, Color, Multiway, Gravity, Collision, Solid')
+		.attr({x: 0, y: 0, w: 50, h: 50})
+		.color('red')
+		.multiway(7, {
+			W: -90,
+			A: 180,
+			D: 0,
+		})
+		.gravity('Solid')
+		.gravityConst(.3)
+		.stopOnSolids();
+	},
+
+	 // Registers a stop-movement function to be called when
+	// this entity hits an entity with the "Solid" component
+	stopOnSolids: function() {
+		console.log('Hey!');
+		this.onHit('Solid', this.stopMovement);
+		return this;
+	},
+
+	// Stops the movement
+	stopMovement: function() {
+		console.log('HI!');
+		this._speed = 0;
+		if (this._movement) {
+			this.x -= this._movement.x;
+			this.y -= this._movement.y;
+		}
+	},
+});
+
+
--- src/game.js
+++ src/game.js
@@ -1,18 +1,21 @@
 Game = {
 	start: function() {
 
-		Crafty.init(800,800);
+		Crafty.init(900,400);
 		Crafty.background('rgb(121,205,205)');
 			
 		Crafty.e('Ground')
-			.attr({x: 0, y: 700, w: 500, h: 300})
+			.attr({x: 200, y: 300, w: 100, h: 10})
 			.color('green');
 
-			
-		Crafty.e('Door1')
-			.attr({x:200, y:100});
+		Crafty.e('Ground')
+			.attr({x: 0, y:350, w: 500, h: 50})
+			.color('green');
 		  
 		Crafty.e('Player1')
-		  .attr({x: 0, y: 699})
+		  	.attr({x: 0, y: 299})
+
+	  	Crafty.e('Player2')
+	  		.attr({x: 100, y: 299})
 	}
  }
