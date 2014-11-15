066d6069e9a9b3f07643fea156e8578d1003da4a
James Kotzian <jamezbond114@gmail.com>
2014-11-15 05:42:11 -0500

    Added grid system for easier level creation

--- src/components.js
+++ src/components.js
@@ -3,6 +3,25 @@
 // An "Actor" is an entity that is drawn in 2D on canvas
 //  via our logical coordinate grid
 
+Crafty.c('Grid', {
+	init: function () {
+		this.attr({
+			w: Game.map_grid.tile.width,
+			h: Game.map_grid.tile.height
+		})
+	},
+
+	  // Locate this entity at the given position on the grid
+  	at: function(x, y) {
+    	if (x === undefined && y === undefined) {
+      		return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
+    	} 
+    	else {
+      		this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
+      		return this;
+    	}
+  	}
+})
 Crafty.c('Actor', {
     init: function() {
         this.requires('2D, Canvas, Grid');
@@ -12,7 +31,7 @@ Crafty.c('Actor', {
 Crafty.c('Ground', {
 	init: function() {
 		this.requires('Actor, Color, Solid')
-			.color('rgb(20,125,40)');
+			.color('green');
 	}
 });
 
--- src/game.js
+++ src/game.js
@@ -1,16 +1,49 @@
 Game = {
+	  // This defines our grid's size and the size of each of its tiles
+	map_grid: {
+		width:  9,
+		height: 9,
+		tile: {
+		  width:  64,
+		  height: 64
+		}
+	},
+
+	// The total width of the game screen. Since our grid takes up the entire screen
+	//  this is just the width of a tile times the width of the grid
+	width: function() {
+		return this.map_grid.width * this.map_grid.tile.width;
+	},
+
+	// The total height of the game screen. Since our grid takes up the entire screen
+	//  this is just the height of a tile times the height of the grid
+	height: function() {
+		return this.map_grid.height * this.map_grid.tile.height;
+	},
+
 	start: function() {
+		Crafty.init(Game.map_grid.width * 
+					Game.map_grid.tile.width,
+					Game.map_grid.height * 
+					Game.map_grid.tile.height);
 
-		Crafty.init(900,400);
 		Crafty.background('rgb(121,205,205)');
-			
-		Crafty.e('Ground')
-			.attr({x: 200, y: 300, w: 100, h: 10})
-			.color('green');
+		
+		//Place some ground on the map
+		for (var x = 0; x < Game.map_grid.width; x++) {
+			for (var y = 0; y < Game.map_grid.height; y++) {
+				if (y == Game.map_grid.height - 1) {
+					Crafty.e('Ground').at(x, y);
+				}
+			}
+		}
+		// Crafty.e('Ground')
+		// 	.attr({x: 200, y: 300, w: 100, h: 10})
+		// 	.color('green');
 
-		Crafty.e('Ground')
-			.attr({x: 0, y:350, w: 1000, h: 50})
-			.color('green');
+		// Crafty.e('Ground')
+		// 	.attr({x: 0, y:350, w: 1000, h: 50})
+		// 	.color('green');
 		  
 		Crafty.e('Player1')
 		  	.attr({x: 0, y: 299})
